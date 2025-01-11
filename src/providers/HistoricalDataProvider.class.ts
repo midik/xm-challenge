a3405ea558f9113ff7ed09fc78502b6b087879d8
mid <midiman@ya.ru>
2025-01-11 03:04:34 +0100

    narrow down fetched data type

--- src/providers/HistoricalDataProvider.class.ts
+++ src/providers/HistoricalDataProvider.class.ts
@@ -14,5 +14,6 @@ export abstract class HistoricalDataProvider {
     this.options = options
   }
 
-  abstract fetchData(companySymbol: string, startDate: string, endDate: string): Promise<any>
+  abstract fetchData(companySymbol: string, startDate: string, endDate: string)
+    : Promise<ReturnType<HistoricalDataAdapter['normalize']>>
 }
